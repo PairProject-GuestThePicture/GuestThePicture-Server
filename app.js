e49cd73338e42544d31f3827b345a6e889ceb38e
Nurfiah Idris <nurfiahidris@Nurfiahs-MacBook-Air.local>
2020-07-24 13:22:47 +0800

    server

--- app.js
+++ app.js
@@ -27,10 +27,13 @@ let rooms = [
 ]
 
 io.on('connect', function(socket){
-  console.log('Someone connected')
+  console.log('Someone connected', socket.id)
   // socket === connected client
   // send data
-  socket.emit('roomsFromServer', rooms)
+  socket.on('roomsFromServer', function (message) {
+    console.log(message)
+    socket.emit('roomsFromServer', rooms)
+  })
   // receive data
   socket.on('newRooms', function(payload){
     rooms.push(payload)
@@ -42,16 +45,27 @@ io.on('connect', function(socket){
   //   //  send to all include its self
   //    socket.emit('')
   })
-  socket.on('joinRoom', function(room){
-    console.log('someone wanna join the room', room)
+  socket.on('joinRoom', function(newRoom){
+    console.log('someone wanna join the room', newRoom)
     rooms.forEach(room=>{
-      if(room.title == room & room.members.length > 4){
-        room.members++
-        socket.join(room.title)
-      }else{
-        let message = 'No empty space'
-        socket.emit('fullMember', message)
+      if(room.title == newRoom){
+        if(room.members.length < 4){
+          room.members.push()
+          socket.emit('responseJoin', newRoom)
+          socket.join(room.title)
+        }else{
+          let message = 'Room is full'
+          socket.emit('fullRoom', message)
+          socket.emit('responseJoin', null)
+          
+        }
       }
+      // if(room.title == newRoom && room.members.length < 4){
+      //   socket.join(room.title)
+      // } else if(room.title == newRoom && room.members.length > 4){
+      //   let messag
+      //   socket.emit('fullMember',message)
+      // }
     })
   })
 });
